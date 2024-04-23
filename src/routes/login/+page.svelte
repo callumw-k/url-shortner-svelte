6d38ffa4ebe1a07b0fc9987536d82bd17793f6b7
Callum Wellwood-Kane <business@callumwk.com>
2024-04-23 22:48:16 +1000

    updated login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,11 @@
 <script>
-	import { SignIn } from "@auth/sveltekit/components";
+	import { buttonVariants } from "$lib/components/ui/button";
+	import { cn } from "$lib/utils";
+  import { SignIn } from "@auth/sveltekit/components";
 </script>
 
-<div>
-  <SignIn provider="google" signInPage="login" />
+<div class="flex justify-center mt-4 px-4">
+  <SignIn className={cn(buttonVariants({ variant: "default", size: "default", className: "" }))} provider="google" signInPage="login" >
+    <span slot="submitButton">Sign in</span>
+  </SignIn>
 </div> 
